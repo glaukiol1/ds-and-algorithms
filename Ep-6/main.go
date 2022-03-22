6c8ec8c47273837dac3ecf2da0267586a8f0b89f
glaukiol1 <glaukiol1@gmail.com>
2022-03-22 23:11:48 +0100

    added fib()

--- Ep-6/main.go
+++ Ep-6/main.go
@@ -4,12 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Fact of 4:", fact(4))
+	fmt.Println("First 8 fibonacci:", fib(8))
 }
 
 func fact(n int) int {
 	// assuming that n is a positive integer or 0
 	if n >= 1 {
 		return n * fact(n-1)
+	}
+	return 1
+}
+
+func fib(n int) int {
+	if n >= 3 {
+		return (fib(n-1) + fib(n-2))
 	}
 	return 1
 }
